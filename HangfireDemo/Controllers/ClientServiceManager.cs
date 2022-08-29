844a79f1fec82e4fcff360a681139d8bad41ceca
Önder ÖZKUM <onderozkum@hotmail.com>
2022-08-29 17:15:47 +0300

    Add project files.

---
+++ HangfireDemo/Controllers/ClientServiceManager.cs
@@ -0,0 +1,20 @@
+﻿namespace HangfireDemo.Controllers
+{
+    public class ClientServiceManager
+    {
+        public static void Loglama()
+        {
+            LogHelper.Log(LogLevel.Info, "Job tetiklendi...");
+            try
+            {
+                string ayrac = new string('-', 6);
+                int a = 100, b = 0;
+                a = (a / b);
+            }
+            catch (Exception e)
+            {
+                LogHelper.Log(LogLevel.Error, e.Message);
+            }
+        }
+    }
+}
---
+++ HangfireDemo/LogHelper.cs
@@ -0,0 +1,133 @@
+﻿using System.Diagnostics;
+using System.Reflection;
+
+namespace HangfireDemo
+{
+    public static class LogHelper
+    {
+        public static void Log(string logMessage)
+        {
+            try
+            {
+                string logLevelDefinition = string.Empty;
+
+                if (string.IsNullOrEmpty(logMessage)) return;
+                string directoryName = Path.GetDirectoryName(AppDomain.CurrentDomain.BaseDirectory);
+                directoryName = string.Concat(directoryName, "\\Logs");
+                if (!Directory.Exists(directoryName))
+                {
+                    Directory.CreateDirectory(directoryName);
+                }
+                DateTime now = DateTime.Now;
+                string str = string.Concat(directoryName, "\\Log_", now.ToString("dd-MM-yyyy"), ".txt");
+                using (var streamWriter = new StreamWriter(str, true))
+                {
+                    streamWriter.WriteLine(string.Concat(DateTime.Now, logLevelDefinition, " ", string.Concat("<!--------------- ", logMessage, " ---------------!>")));
+                    streamWriter.Close();
+                    streamWriter.Dispose();
+                }
+            }
+            catch (Exception exception)
+            {
+                EventLog.WriteEntry(nameof(LogHelper), exception.Message, EventLogEntryType.Error);
+            }
+        }
+        public static void Log(LogLevel logLevel, string logMessage)
+        {
+            try
+            {
+                string logLevelDefinition = string.Empty;
+                switch (logLevel)
+                {
+                    case LogLevel.Application:
+                        logLevelDefinition = "[Application]";
+                        break;
+                    case LogLevel.Error:
+                        logLevelDefinition = "[ERROR]:";
+                        break;
+                    case LogLevel.Warning:
+                        logLevelDefinition = "[WARN]:";
+                        break;
+                    case LogLevel.Info:
+                        logLevelDefinition = "[INFO]:";
+                        break;
+                    case LogLevel.Question:
+                        logLevelDefinition = "[INFO]:";
+                        break;
+                }
+                if (string.IsNullOrEmpty(logMessage)) return;
+                string directoryName = Path.GetDirectoryName(AppDomain.CurrentDomain.BaseDirectory);
+                directoryName = string.Concat(directoryName, "\\Logs");
+                if (!Directory.Exists(directoryName))
+                {
+                    Directory.CreateDirectory(directoryName);
+                }
+                DateTime now = DateTime.Now;
+                string str = string.Concat(directoryName, "\\Log_", now.ToString("dd-MM-yyyy"), ".txt");
+                using (var streamWriter = new StreamWriter(str, true))
+                {
+                    streamWriter.WriteLine(string.Concat(DateTime.Now, logLevelDefinition, " ", logMessage));
+                    streamWriter.Close();
+                    streamWriter.Dispose();
+                }
+            }
+            catch (Exception exception)
+            {
+                EventLog.WriteEntry(nameof(LogHelper), exception.Message, EventLogEntryType.Error);
+            }
+        }
+        public static void Log(LogLevel logLevel, Type currentClass, MethodBase currentMethod, string logMessage)
+        {
+            try
+            {
+                string logLevelDefinition = string.Empty;
+                switch (logLevel)
+                {
+                    case LogLevel.Application:
+                        logLevelDefinition = "[Application]";
+                        break;
+                    case LogLevel.Error:
+                        logLevelDefinition = "[ERROR]:";
+                        break;
+                    case LogLevel.Warning:
+                        logLevelDefinition = "[WARN]:";
+                        break;
+                    case LogLevel.Info:
+                        logLevelDefinition = "[INFO]:";
+                        break;
+                    case LogLevel.Question:
+                        logLevelDefinition = "[INFO]:";
+                        break;
+                }
+                if (string.IsNullOrEmpty(logMessage)) return;
+                string directoryName = Path.GetDirectoryName(AppDomain.CurrentDomain.BaseDirectory);
+                directoryName = string.Concat(directoryName, "\\Logs");
+                if (!Directory.Exists(directoryName))
+                {
+                    Directory.CreateDirectory(directoryName);
+                }
+                DateTime now = DateTime.Now;
+                string str = string.Concat(directoryName, "\\Log_", now.ToString("dd-MM-yyyy"), ".txt");
+                using (var streamWriter = new StreamWriter(str, true))
+                {
+                    streamWriter.WriteLine(string.Concat(DateTime.Now, logLevelDefinition, " ", currentClass?.Name, " ", currentMethod?.Name, " ", logMessage));
+                    streamWriter.Close();
+                    streamWriter.Dispose();
+                }
+            }
+            catch (Exception exception)
+            {
+                EventLog.WriteEntry(nameof(LogHelper), exception.Message, EventLogEntryType.Error);
+            }
+
+        }
+    }
+    public enum LogLevel
+    {
+        Application,
+        Error,
+        Warning,
+        Info,
+        Question
+    }
+}
